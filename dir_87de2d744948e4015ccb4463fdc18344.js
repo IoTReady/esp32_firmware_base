93df852bd10c5ceaec5de39f94fb724e41bb6c86
hairyhoncho <jay.warrior@mobiatrics.com>
2021-03-17 21:41:07 -0700

    More documentation

---
+++ dir_87de2d744948e4015ccb4463fdc18344.js
@@ -0,0 +1,6 @@
+var dir_87de2d744948e4015ccb4463fdc18344 =
+[
+    [ "main.c", "wifi__config__rest_2main_2main_8c.html", "wifi__config__rest_2main_2main_8c" ],
+    [ "tasks.c", "wifi__config__rest_2main_2tasks_8c.html", "wifi__config__rest_2main_2tasks_8c" ],
+    [ "tasks.h", "wifi__config__rest_2main_2tasks_8h.html", "wifi__config__rest_2main_2tasks_8h" ]
+];
---
+++ search/pages_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['wifi_20config_20rest_20example_153',['WiFi Config Rest Example',['../md_examples_wifi_config_rest__r_e_a_d_m_e.html',1,'']]],
+  ['wifi_20softap_20example_154',['WiFi SoftAP Example',['../md_examples_wifi_softap__r_e_a_d_m_e.html',1,'']]],
+  ['wifi_20station_20example_155',['WiFi Station Example',['../md_examples_wifi_station__r_e_a_d_m_e.html',1,'']]]
+];
